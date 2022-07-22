868b11ffff273bc03c308c3f7804323493a8941e
isacazma <78527260+isacazma@users.noreply.github.com>
2022-07-22 17:21:25 +0200

    17.03

---
+++ src/main/java/nl/hu/bep/battlesnake/parsers/MoveResponse.java
@@ -0,0 +1,11 @@
+//package nl.hu.bep.battlesnake.parsers;
+//
+//public class MoveResponse{
+//    public String move;
+//    public  String shout;
+//
+//    public  MoveResponse(String move , String shout){
+//        this.move = move;
+//        this.shout = shout;
+//    }
+//}
