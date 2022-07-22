f8745d10b7aadc7cb455048104b3dc6c9a68d449
isacazma <78527260+isacazma@users.noreply.github.com>
2022-07-22 17:25:38 +0200

    17.03

--- src/main/java/nl/hu/bep/battlesnake/parsers/MoveResponse.java
+++ src/main/java/nl/hu/bep/battlesnake/parsers/MoveResponse.java
@@ -9,3 +9,31 @@
 //        this.shout = shout;
 //    }
 //}
+
+//import javax.ws.rs.Consumes;
+//import javax.ws.rs.POST;
+//import javax.ws.rs.Path;
+//import javax.ws.rs.core.MediaType;
+//
+//@POST
+//@Path("/move")
+//@Consumes(MediaType.APPLICATION_JSON)
+//public Response moveSnake(String r){
+//        System.out.println(r);
+//        ObjectMapper om = new ObjectMapper();
+//        try {
+//        JsonNode request = om.readTree(r);
+//        Board board = new Board(request.get("board"));
+//        for (JsonNode node : request.get("board").get("snakes")) {
+//        board.addSnake(new Snake(node));
+//        }
+//        System.out.println(board.getSnakes().get(0).getName());
+//        MoveResponse move = new MoveResponse("up","going Up");
+//
+//        return  Response.ok(move).build();
+//        } catch (JsonProcessingException e) {
+//        e.printStackTrace();
+//        }
+//        return  Response.serverError().build();
+//        }
+
