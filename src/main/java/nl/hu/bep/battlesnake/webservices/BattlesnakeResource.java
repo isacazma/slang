c451805b535240833cc4f2252e5192495187eb39
isacazma <78527260+isacazma@users.noreply.github.com>
2022-07-22 21:19:45 +0200

    20:50

--- src/main/java/nl/hu/bep/battlesnake/webservices/BattlesnakeResource.java
+++ src/main/java/nl/hu/bep/battlesnake/webservices/BattlesnakeResource.java
@@ -60,9 +60,10 @@ public class BattlesnakeResource {
 
         System.out.println(board.getSnakes().get(0).getName());
             System.out.println(board.getSnakes().get(0).getBody());
+            System.out.println(board.getSnakes().get(0).getHead());
             System.out.println(board.getSnakes().get(0).getHead().x);
-        MoveResponse move = new MoveResponse("up","going Up");
-
+            System.out.println(board.getSnakes().get(0).getHead().y);
+            MoveResponse move = new MoveResponse("up","going Up");
 
             return  Response.ok(move).build();
         } catch (JsonProcessingException e) {
