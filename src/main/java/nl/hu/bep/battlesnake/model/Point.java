112d5d69a49a8fe9aa1d3866a2fb5620b950d4f2
isacazma <78527260+isacazma@users.noreply.github.com>
2022-08-30 07:26:34 +0200

    20:49

---
+++ src/main/java/nl/hu/bep/battlesnake/model/Point.java
@@ -0,0 +1,12 @@
+package nl.hu.bep.battlesnake.model;
+
+public class Point {
+
+    public int x;
+    public int y;
+
+    public Point(int xd, int yd) {
+        this.x = xd;
+        this.y = yd;
+    }
+}
