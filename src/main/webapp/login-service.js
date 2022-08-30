215ff3b14fa700f051ca0711461bded442bc077c
isacazma <78527260+isacazma@users.noreply.github.com>
2022-08-30 05:24:15 +0200

    20:49

--- src/main/webapp/login-service.js
+++ src/main/webapp/login-service.js
@@ -1,23 +1,47 @@
 export default class LoginService {
     isLoggedIn() {
-        //TODO: hoe ga je bepalen of iemand ingelogd is (geweest)?
-        return false;
+        let token = window.localStorage.getItem("abc");
+        if (token) {
+            return true;
+        } else {
+            return false;
+        }
     }
+    //TODO: hoe ga je bepalen of iemand ingelogd is (geweest)?
 
-    login(user, password) {
-        //TODO: inloggen met POST
-        return Promise.resolve();
+    login(username, password) {
+        console.log("login")
+
+        var formData = new FormData();
+        formData.append("username", username);
+        formData.append("password", password);
+        var encData = new URLSearchParams(formData.entries());
+
+
+        fetch("/restservices/authentication", {
+            method: "POST",
+            headers: {
+                'Content-Type':'application/x-www-form-urlencoded;charset=UTF-8'
+            },
+            body: encData
+        })
+            .then((response) => response.json())
+            .then(data => window.localStorage.setItem("abc", data['JWT']));
     }
 
     getUser() {
         //TODO: deze GET method test je token op server-side problemen. Je kunt client-side op zich wel 'ingelogd' zijn
         //maar het zou altijd zomaar kunnen dat je token verlopen is, of dat er server-side iets anders aan de hand is.
         //Dus het is handig om te checken met een -echte fetch- of je login-token wel echt bruikbaar is.
-        return Promise.resolve(null);
+        return fetch("/restservices/snake", {
+            method: "GET",
+            headers: { "Authorization": "Bearer " + window.localStorage.getItem("abc") },
+            credentials: "include"
+        })
+            .then(data => console.log(data));
     }
 
     logout() {
-        //TODO: hoe ga je eigenlijk iemand 'uitloggen'?
-        return Promise.resolve();
+        //TODO: Logout
     }
 }
