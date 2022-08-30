2020503f2319bc7831bd8ea8d36b049f27f5d4ed
isacazma <78527260+isacazma@users.noreply.github.com>
2022-08-30 06:56:59 +0200

    20:49

--- src/main/webapp/login-service.js
+++ src/main/webapp/login-service.js
@@ -33,7 +33,7 @@ export default class LoginService {
         //TODO: deze GET method test je token op server-side problemen. Je kunt client-side op zich wel 'ingelogd' zijn
         //maar het zou altijd zomaar kunnen dat je token verlopen is, of dat er server-side iets anders aan de hand is.
         //Dus het is handig om te checken met een -echte fetch- of je login-token wel echt bruikbaar is.
-        return fetch("/restservices/snake/", {
+        return fetch("/restservices/snake", {
             method: "GET",
             headers: { "Authorization": "Bearer " + window.localStorage.getItem("abc") },
             credentials: "include"
