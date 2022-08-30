f2599834acf4f2d2c18aa3b153625b7cbc7622b3
isacazma <78527260+isacazma@users.noreply.github.com>
2022-08-30 06:00:01 +0200

    20:49

--- src/main/java/nl/hu/bep/battlesnake/movement/DataParser.java
+++ src/main/java/nl/hu/bep/battlesnake/movement/DataParser.java
@@ -4,12 +4,15 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import nl.hu.bep.battlesnake.model.Board;
+import nl.hu.bep.battlesnake.model.Game;
 import nl.hu.bep.battlesnake.model.Snake;
 
 // Move parser will receive json input and will parse all fields to a board object
 public class DataParser {
     private Board board;
     private Snake you;
+    private Game game;
+    private JsonNode turn;
 
     public void DataParse(String r) throws JsonProcessingException {
         ObjectMapper om = new ObjectMapper();
@@ -17,6 +20,8 @@ public class DataParser {
             JsonNode request = om.readTree(r);
             this.board = new Board(request.get("board"));
             this.you = new Snake(request.get("you"));
+            this.game = new Game(request.get("game"));
+            this.turn = request.get("turn");
 
             for (JsonNode node : request.get("board").get("snakes")) {
                 board.addSnake(new Snake(node));
@@ -32,6 +37,22 @@ public class DataParser {
 
     public void setBoard(Board board) {
         this.board = board;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public JsonNode getTurn() {
+        return turn;
+    }
+
+    public void setTurn(JsonNode turn) {
+        this.turn = turn;
     }
 
     public Snake getYou() {
